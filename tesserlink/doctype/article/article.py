46ea2c2db468fd518be3e9691a9b568d51c3af1b
Your Name <singhanmolp18@gmail.com>
2024-08-29 13:03:47 -0700

    LMS

---
+++ tesserlink/doctype/article/article.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, me and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.website.website_generator import WebsiteGenerator
+
+
+class Article(WebsiteGenerator):
+	pass
